4efecfb6f6521cebe8d89217fd1f1557a2d7896a
Korelov Maksim <korelov@gmail.com>
2021-10-09 22:15:16 +0300

    add FizzBuzz task and test

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class FizzBuzz {
+
+    public static String[] fizzBuzz(int startNumber, int endNumber) {
+        String[] numbers = new String[endNumber - startNumber + 1];
+        int newNumber = startNumber;
+        for (int i = startNumber - startNumber; i < (endNumber - startNumber + 1); i++) {
+
+            if (newNumber % 15 == 0) {
+                numbers[i] = "FizzBuzz";
+            } else if (newNumber % 3 == 0) {
+                numbers[i] = "Fizz";
+            } else if (newNumber % 5 == 0) {
+                numbers[i] = "Buzz";
+            } else {
+                numbers[i] = String.valueOf(newNumber);
+            }
+            newNumber += 1;
+        }
+
+
+//        int[] numbers1 = new int[endNumber - startNumber + 1];
+//        int indexArr = 0;
+//        for (int i = startNumber; i <= endNumber; i++) {
+//            numbers1[indexArr++] = i;
+//        }
+        System.out.println(Arrays.toString(numbers));
+        return numbers;
+
+
+    }
+
+
+
+
+}
