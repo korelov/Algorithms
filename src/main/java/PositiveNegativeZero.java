cb00534d93a352a7262a01df2ff6d138c67ddf2a
Korelov Maksim <korelov@gmail.com>
2021-11-02 22:03:25 +0300

    PositiveNegativeZero HW_11_12 10/10

---
+++ src/main/java/PositiveNegativeZero.java
@@ -0,0 +1,9 @@
+public class PositiveNegativeZero {
+
+    public boolean isPositiveNegativeZero(int number) {
+        if (number >= 0) {
+            return true;
+        }
+        return false;
+    }
+}
