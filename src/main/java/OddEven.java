fbd9483987364c5b030a639e3cf735c1661f1542
Korelov Maksim <korelov@gmail.com>
2021-11-02 21:59:11 +0300

    OddEven  HW_11_12 1/10

--- src/main/java/OddEven.java
+++ src/main/java/OddEven.java
@@ -1,25 +1,14 @@
 public class OddEven {
 
-    public  String oddEven(int number){
+    public String oddEven(int number) {
 
-        if (number%2!=0){
-            return "Odd";
+        if (number <= Integer.MAX_VALUE && number >= Integer.MIN_VALUE) {
+            if (number % 2 == 0) {
+                return "Even";
+            } else if (Math.abs(number % 2) == 1) {
+                return "Odd";
+            }
         }
-        if (number%2==0){
-            return "Even";
-        }
-        if (number > Integer.MAX_VALUE ){
-            return "Undefined";
-        } else {
-            return "Undefined";
-        }
+        return "Undefined";
     }
-
-//    public static void main(String[] args) {
-//        System.out.println(oddEven(-345));
-//        System.out.println(oddEven(0));
-//        System.out.println(oddEven(222_222));
-//        System.out.println(oddEven((11)));
-//    }
-
 }
--- src/test/java/OddEvenTest.java
+++ src/test/java/OddEvenTest.java
@@ -3,14 +3,49 @@ import org.junit.Test;
 
 public class OddEvenTest {
 
-    @Test
-    public void testOddEven(){
-        String odd = "Odd";
-        String evem = "Even";
-        String undefined = "Undefined";
+    private String odd = "Odd";
+    private String evem = "Even";
+    private String undefined = "Undefined";
 
+    @Test
+    public void testOddEven() {
         OddEven test = new OddEven();
-        Assert.assertEquals(odd,test.oddEven(2));
+        Assert.assertEquals(odd, test.oddEven(345));
     }
 
+    @Test
+    public void testOddNegat() {
+        OddEven test = new OddEven();
+        Assert.assertEquals(odd, test.oddEven(-345));
+    }
+
+    @Test
+    public void testZero() {
+        OddEven oddEven = new OddEven();
+        Assert.assertEquals(evem, oddEven.oddEven(0));
+    }
+
+    @Test
+    public void testEvenNegativeNumber() {
+        OddEven oddEven = new OddEven();
+        Assert.assertEquals(evem, oddEven.oddEven(-2222));
+    }
+
+    @Test
+    public void testUndefinedMoreThanMax() {
+        long number = 2147483648L;
+        String expectedResult = "Undefined";
+        OddEven oddEven = new OddEven();
+        String actualResult = oddEven.oddEven((int) 2147483648L);
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testUndefinedLessThanMin() {
+        long number = -2147483649L;
+        String expectedResult = "Undefined";
+        OddEven oddEven = new OddEven();
+        String actualResult = oddEven.oddEven((int) number);
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 }
